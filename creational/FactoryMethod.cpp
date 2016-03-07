b7121eb8a1bc23b158b880311a768b7c04b7c9ad
Rokuren <rokuren.m@gmail.com>
2016-03-07 13:54:51 -0500

    Factory Method
    Changed filename, added details on design pattern

--- creational/FactoryMethod.cpp
+++ creational/FactoryMethod.cpp
@@ -1,3 +1,8 @@
+/*
+ * FACTORY METHOD
+ *  Defines an interface for creating an object, but let's the subclass decide which class to 
+ *   instantiate. Factory Method lets a class defer instantiation to subclasses.
+ *  Defines a virtual constructor.
 #include <iostream>
 
 /* Abstract base class declared by framework */
